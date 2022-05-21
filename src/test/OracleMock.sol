39b96fb676e13446e4a9f3bf150af45c183f925e
oraclown <owen.burton13@gmail.com>
2022-05-21 11:18:16 -0400

    Adjust return type

--- src/test/OracleMock.sol
+++ src/test/OracleMock.sol
@@ -2,9 +2,7 @@
 pragma solidity 0.8.10;
 
 contract OracleMock {
-    constructor() public {}
-
-    function rng() public view returns (bytes32) {
+    function getCurrentValue(bytes32) public view returns (bytes32) {
         return blockhash(block.number - 1);
     }
 }
